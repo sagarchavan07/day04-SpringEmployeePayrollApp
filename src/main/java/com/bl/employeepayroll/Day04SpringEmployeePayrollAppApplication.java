09a27ef8917cf17cdbf91a074858bd68af7758ea
sagarchavan07 <chavansagar96044@gmail.com>
2022-09-02 17:58:30 +0530

    initial commit

---
+++ src/main/java/com/bl/employeepayroll/Day04SpringEmployeePayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.bl.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day04SpringEmployeePayrollAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day04SpringEmployeePayrollAppApplication.class, args);
+    }
+
+}
